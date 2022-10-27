ebb7cf769772a69f3e5f6162d7e832400eabbd7e
Ashley Hall <hallashley781.hallashley781@gmail.com>
2022-10-27 14:17:39 -0700

    Added coding assginment files

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+Inside index.js, create one variable for each of the following real-life examples and
+assign them values:
+a. Item price
+b. Amount of money in wallet
+c. Number of friends
+d. Age in years (as a whole number)
+e. First name
+f. Last name
+g. Middle initial
+
+*/ 
+let middleIntial= 'L'; //declare the variable equal to a string of L
+let firstName= 'Ashley'
+let lastName= 'Hall'
+    console.log(middleIntial+ ' '+ 'Leann' + ' ' + firstName + ' ' + lastName);
+
